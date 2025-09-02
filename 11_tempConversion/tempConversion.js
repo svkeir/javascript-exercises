27d7dfbad33384bc4204c8c8e6acc323bb224471
skeir <169955861+svkeir@users.noreply.github.com>
2025-09-02 13:12:11 -0300

    exercise

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  return Math.round((tempF - 32) * (5 / 9) * 10) / 10;
+
 };
 
-const convertToFahrenheit = function() {
-};
+const convertToFahrenheit = function(tempC) {
+     return Math.round(((tempC * 9) / 5 + 32) * 10) / 10;
+}
+  
+
+// (tempC × 9/5) + 32 = 32°F (to Fahrenheit)
+
+//(tempF − 32) × 5/9 = -17.78°C (to Celsius)
+
+// originalNumber.toFixed(1) (to round to one decimal)
+
+
 
 // Do not edit below this line
 module.exports = {
